e028a17bf8e7c796ce372e46e12edeecf843d379
Reese Alford <reesecalford@yahoo.com>
2019-11-23 16:36:45 -0500

    Path testing

---
+++ src/main/java/frc/robot/PathFollowingConstants.java
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class PathFollowingConstants {
+    public static final double kWheelDiameter = 0.0;
+    public static final double kMaxVelocity = 0.0;
+    public static final double kTicksPerRev = 0.0;
+    public static final String pathName = "Auto";
+}
